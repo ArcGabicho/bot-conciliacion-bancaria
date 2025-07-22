17f2037c6f51a6e40e1407abcc7940a0e04eae7a
ArcGabo <pcjhonny13@gmail.com>
2025-07-22 11:55:19 -0500

    Agregando ASCII

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 #include "./include/bot_conciliacion.h"
 
 int main() {
-    std::cout << "Iniciando Bot de Conciliación Bancaria Automática...\n";
+    std::cout << R"(
+     ____              _      _____                           _ _ _       _   _
+    |  _ \            | |    |  __ \                         (_) (_)     | | (_)
+    | |_) | __ _ _ __ | | __ | |__) |___  ___ ___  _ __   ___ _| |_  __ _| |_ _  ___  _ __
+    |  _ < / _` | '_ \| |/ / |  _  // _ \/ __/ _ \| '_ \ / __| | | |/ _` | __| |/ _ \| '_ \
+    | |_) | (_| | | | |   <  | | \ \  __/ (_| (_) | | | | (__| | | | (_| | |_| | (_) | | | |
+    |____/ \__,_|_| |_|_|\_\ |_|  \_\___|\___\___/|_| |_|\___|_|_|_|\__,_|\__|_|\___/|_| |_|
+    )";
+
+    std::cout << "Iniciando Bot de Conciliacion Bancaria Automatica...\n";
 
     std::string rutaInterno = "../data/interno.csv";
     std::string rutaBanco = "../data/banco.csv";
